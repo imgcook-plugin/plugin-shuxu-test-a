42cf08e955887cd903d8e9f82a2373ea2399be27
BigGress <t451148156@gmail.com>
2021-04-01 16:10:18 +0800

    test

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 
 const pluginHandler = async options => {
   let { data, filePath, config  } = options;
+  options.data.code.panelDisplay[0].panelValue = `${options.data.code.panelDisplay[0].panelValue}`.replace('想看', '哈哈哈哈会');
   // body...
   return { data, filePath, config };
 };
