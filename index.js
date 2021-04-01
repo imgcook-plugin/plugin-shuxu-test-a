02f906f300e2f78e2329c6c35ee77836c0bb228b
BigGress <t451148156@gmail.com>
2021-04-01 17:13:31 +0800

    save

--- index.js
+++ index.js
@@ -8,7 +8,9 @@
 
 const pluginHandler = async options => {
   let { data, filePath, config  } = options;
-  options.data.code.panelDisplay[0].panelValue = `${options.data.code.panelDisplay[0].panelValue}`.replace('想看', '哈哈哈哈会');
+  options.data.code.panelDisplay[0].panelValue = `${options.data.code.panelDisplay[0].panelValue}`.replace(/想看/g, '哈哈哈哈会');
+
+  options.data.code.xml = `${options.data.code.xml}`.replace(/想看/g, '哈哈哈哈会');
   // body...
   return { data, filePath, config };
 };
