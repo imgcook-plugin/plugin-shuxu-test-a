5581dfd306e306efc046a117ee00d434b93241e6
shuxu <451148156@qq.com>
2021-04-06 14:18:23 +0800

    feat: test

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ const pluginHandler = async options => {
   // options.data.code.panelDisplay[0].panelValue = `${options.data.code.panelDisplay[0].panelValue}`.replace('想看', '哈哈哈哈会');
 
   data.xml = handler.default(options);
+
+  options.moduleData.json = options.moduleData.json.replace(/影片名/g, '打算粉红色的');
+  options.moduleData.jsonv2 = options.moduleData.jsonv2.replace(/影片名/g, '打算粉红色的');
   // body...
   return { data, filePath, config };
 };
