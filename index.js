6864e4b8ff5b7fdcd6bcdc63604b537bfcd3be80
shuxu <451148156@qq.com>
2021-04-06 18:38:51 +0800

    feat: test

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const handler = require('./dist/index');
  */
 
 const pluginHandler = async options => {
-  let { data, filePath, config  } = options;
+  // let { data, filePath, config  } = options;
   // options.data.code.panelDisplay[0].panelValue = `${options.data.code.panelDisplay[0].panelValue}`.replace('想看', '哈哈哈哈会');
 
   const newOptions = handler.default(options);
